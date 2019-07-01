b0f0e874eb259406871d72a791cc588a54366d80
romigos <bolist@mail.ru>
2019-07-01 13:31:21 +0300

    create 2 model: cart and user, make 2 migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_626_122_259) do
-  create_table 'items', force: :cascade do |t|
-    t.float 'price'
-    t.string 'name'
-    t.boolean 'real'
-    t.float 'weight'
-    t.string 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'votes_count', default: 0
+ActiveRecord::Schema.define(version: 2019_06_27_143759) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "items", force: :cascade do |t|
+    t.float "price"
+    t.string "name"
+    t.boolean "real"
+    t.float "weight"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "votes_count", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
